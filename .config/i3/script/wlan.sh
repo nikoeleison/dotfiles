2c49def584a0e90a747005d0b8766e284be50c99
Niko Eleison Sarisno <nikoeleison@gmai.com>
2019-10-08 21:59:16 +0700

    reformatting output

--- .config/i3/script/wlan.sh
+++ .config/i3/script/wlan.sh
@@ -1,9 +1,14 @@
 # @author nikoeleison
 # !/bin/bash
 
-download=`ifstat wlp3s0 | grep wlp3s0 | awk '{print $2}'`
-upload=`ifstat wlp3s0 | grep wlp3s0 | awk '{print $3}'`
-output=": $download : $upload"
+status=$(cat /sys/class/net/wlp3s0/carrier)
+if [ $status == "1" ]; then
+	download=`ifstat wlp3s0 | grep wlp3s0 | awk '{print $2}'`
+	upload=`ifstat wlp3s0 | grep wlp3s0 | awk '{print $3}'`
+	output=" $download B/s  $upload B/s"
+else
+	output="N/A"
+fi
 
 echo " $output"
 exit 0
