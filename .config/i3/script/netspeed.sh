8f07c840a4f701ee811ffff4c758f861429b89f5
Niko Eleison Sarisno <nikoeleison@gmai.com>
2019-10-08 22:06:51 +0700

    reformatting output

--- .config/i3/script/netspeed.sh
+++ .config/i3/script/netspeed.sh
@@ -7,14 +7,13 @@ isWlan=$(cat /sys/class/net/wlp3s0/carrier)
 if [ $isEth == "1" ]; then
 	download=`ifstat enp2s0 | grep enp2s0 | awk '{print $2}'`
 	upload=`ifstat enp2s0 | grep enp2s0 | awk '{print $3}'`
-	output=" $download B/s  $upload B/s"
+	echo "  $download B/s  $upload B/s"
 elif [ $isWlan == "1" ]; then
 	download=`ifstat wlp3s0 | grep wlp3s0 | awk '{print $2}'`
 	upload=`ifstat wlp3s0 | grep wlp3s0 | awk '{print $3}'`
-	output=" $download B/s  $upload B/s"
+	echo "  $download B/s  $upload B/s"
 else
-	output="N/A"
+	echo "N/A"
 fi
 
-echo " $output"
 exit 0
