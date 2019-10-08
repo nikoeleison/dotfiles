30968ad7c9a813c892f2e1d7cb5a5ad52cd42d49
Niko Eleison Sarisno <nikoeleison@gmai.com>
2019-10-08 22:00:25 +0700

    added netspeed

---
+++ .config/i3/script/netspeed.sh
@@ -0,0 +1,20 @@
+# @author nikoeleison
+# !/bin/bash
+
+isEth=$(cat /sys/class/net/enp2s0/carrier)
+isWlan=$(cat /sys/class/net/wlp3s0/carrier)
+
+if [ $isEth == "1" ]; then
+	download=`ifstat enp2s0 | grep enp2s0 | awk '{print $2}'`
+	upload=`ifstat enp2s0 | grep enp2s0 | awk '{print $3}'`
+	output=" $download B/s  $upload B/s"
+elif [ $isWlan == "1" ]; then
+	download=`ifstat wlp3s0 | grep wlp3s0 | awk '{print $2}'`
+	upload=`ifstat wlp3s0 | grep wlp3s0 | awk '{print $3}'`
+	output=" $download B/s  $upload B/s"
+else
+	output="N/A"
+fi
+
+echo " $output"
+exit 0
